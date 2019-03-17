3d20297977fc2f58b0af74ddf538d2019104924c
Richard Lindner <richard.lnnr@gmail.com>
2019-03-17 20:30:51 -0300

    Added menu component

--- projects/weather-now/src/app/app.component.spec.ts
+++ projects/weather-now/src/app/app.component.spec.ts
@@ -3,6 +3,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { WeatherCardComponent } from './weather-card/weather-card.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MenuComponent } from './menu/menu.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -13,6 +14,7 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent,
+        MenuComponent,
         WeatherCardComponent
       ],
     }).compileComponents();
--- projects/weather-now/src/app/app.module.ts
+++ projects/weather-now/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { WeatherCardComponent } from './weather-card/weather-card.component';
 import { NgxCurrentWeatherService } from '@weather-lib/ngx-api';
 import { HttpClientModule } from '@angular/common/http';
 import { WeatherComponent } from './weather/weather.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WeatherCardComponent,
-    WeatherComponent
+    WeatherComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
