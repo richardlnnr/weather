a61ba65caadf623b44b98a98c9aca65759b6804c
Richard Lindner <richard.lnnr@gmail.com>
2019-03-17 18:17:23 -0300

    Added footer strutrure and css Added color classes

--- projects/weather-now/src/app/weather-card/weather-card.component.ts
+++ projects/weather-now/src/app/weather-card/weather-card.component.ts
@@ -34,4 +34,28 @@ export class WeatherCardComponent implements OnInit {
       );
   }
 
+  getColorText(temperature: number): string {
+    let textColor = '';
+    // Keep temperature em Kelvin to format in Celsius or Fahrenheit
+    if (temperature <= 278.15) {
+      textColor = 'text-blue';
+    } else if (temperature > 278.15 && temperature <= 298.15) {
+      textColor = 'text-orange';
+    } else {
+      textColor = 'text-red';
+    }
+
+    return textColor;
+  }
+
+  getConvertedTemperature(temperature: number): number {
+    let formatedTemperature = 0;
+    // Kelvin to Celsius
+    formatedTemperature = (temperature - 273.15);
+    // Kelvin to Fahrenheit
+    // formatedTemperature = (temperature - 273.15) * 9 / 5 + 32;
+
+    return Math.round(formatedTemperature);
+  }
+
 }
